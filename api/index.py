d51d23deac10ace244e0b2a586b9c7b59e18e897
Пользователь <nikita.fofan@gmail.com>
2025-11-24 12:07:05 +0300

    Vercel overhaul4

--- api/index.py
+++ api/index.py
@@ -1128,7 +1128,7 @@ def proxy_request(source_label, upstream_path_suffix):
             if is_deepseek and resp.status_code == 200:
                 final_generator = stream_deepseek_refinement(final_generator, prefill_used)
 
-            return Response(stream_with_context(final_generator), resp.status_code, headers, mimetype='text/event-stream')
+            return Response(final_generator, resp.status_code, headers, mimetype='text/event-stream')
         else:
             content = resp.content
             
